58476ea16a03b0cf48986a3a0a7aebf46c762fad
Londot <bryan.londot@hotmail.com>
2020-05-14 12:15:51 +0200

    envoi d'une variable

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('LaBladeDesNuls',function(){
+    $Burns='je suis Mr.Burns';
+    return view('LaBladeDesNuls',compact('Burns'));
+});
